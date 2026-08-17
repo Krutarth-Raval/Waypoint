e01af7e09167d2a4f40b624cfeb68cd03d7dd809
Krutarth Raval <ravalkrutarth95@gmail.com>
2026-08-17 11:48:59 +0530

    chore: bump version and fix android icon/login

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,12 +7,7 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://waypoint-tasks.vercel.app',
     cleartext: true
-  },
-  plugins: {
-    CapacitorCookies: {
-      enabled: true,
-    },
-  },
+  }
 };
 
 export default config;
